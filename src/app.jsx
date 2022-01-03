fead14a19264561291753c5bcb3efafd303a6a26
hyerim <hyyrim@gmail.com>
2022-01-03 16:19:14 +0900

    show video list

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
+import data from './videoList.json';
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+	const [videos, setVideos] = useState([]);
+
+	/*
+  useEffect(() => {const requestOptions = {
+		method: 'GET',
+		redirect: 'follow',
+	};
+
+	fetch(
+		`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=playlist&type=video&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+		requestOptions
+	)
+		.then((response) => response.json())
+		.then((result) => console.log(result))
+		.catch((error) => console.log('error', error))}, [])  
+	
+  */
+	// console.log(data.items);
+	useEffect(() => setVideos(data.items), []);
+
+	return <VideoList videos={videos} />;
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './video_item.module.css';
+
+const VideoItem = ({ video }) => {
+	return (
+		<li className={styles.container}>
+			<div className={styles.video}>
+				<img
+					className={styles.thumbnail}
+					src={video.snippet.thumbnails.medium.url}
+					alt="thumbnail"
+				/>
+				<div className={styles.metadata}>
+					<p className={styles.title}>{video.snippet.title}</p>
+					<p className={styles.channel}>{video.snippet.channelTitle}</p>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+export default VideoItem;
