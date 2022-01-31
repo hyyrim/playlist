2dfe58e43a7957a8b01fd7e72d905895da1e0305
hyerim <hyyrim@gmail.com>
2022-01-31 18:07:38 +0900

    fix video player Error

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './video_detail.module.css';
 
 const VideoDetail = ({ video }) => {
+	console.log(video.id);
 	return (
 		<section className={styles.detail}>
 			<iframe
@@ -9,7 +10,7 @@ const VideoDetail = ({ video }) => {
 				type="text/html"
 				width="100%"
 				height="450px"
-				src={`https://www.youtube.com/embed/${video.id.videoId}`}
+				src={`https://www.youtube.com/embed/${video.id}`}
 				frameBorder="0"
 				allowFullScreen
 			></iframe>
